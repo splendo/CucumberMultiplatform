ab759e31032ca1f023753e715d12a27bedc5bb1d
corrado4eyes <corradoquattrocchi@hotmail.it>
2023-06-23 12:31:27 +0200

    Remove agp version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,9 @@
-import org.apache.tools.ant.taskdefs.condition.Os
-import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
-import org.jetbrains.kotlin.konan.target.KonanTarget
-
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.1.0-beta04").apply(false)
     id("com.android.library").version("8.1.0-beta04").apply(false)
     kotlin("android").version("1.8.20").apply(false)
     kotlin("multiplatform").version("1.8.20").apply(false)
-}
-
-buildscript {
-    repositories {
-        mavenLocal()
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0-rc01")
-    }
 }
 
 tasks.register("clean", Delete::class) {
