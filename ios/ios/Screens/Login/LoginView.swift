2397fcef70f70a27a7a14750634243218c30f7cf
corrado4eyes <corradoquattrocchi@hotmail.it>
2023-06-27 11:18:49 +0200

    Add ios login view

---
+++ ios/ios/Screens/Login/LoginView.swift
@@ -0,0 +1,62 @@
+//
+//  LoginView.swift
+//  ios
+//
+//  Created by Corrado Quattrocchi on 26/06/2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct LoginView: SwiftUI.View {
+    @ObservedObject var emailText: StringSubject
+    @ObservedObject var passwordText: StringSubject
+    
+    @ObservedObject var emailErrorText: StringObservable
+    @ObservedObject var passwordErrorText: StringObservable
+    @ObservedObject var formFooterErrorText: StringObservable
+    
+    @ObservedObject var isLoading: BoolObservable
+    @ObservedObject var isButtonEnabled: BoolObservable
+    
+    private let viewModel: LifecycleViewModel<LoginViewModel>
+    
+    init() {
+        let loginViewModel = LoginViewModel()
+        viewModel = LifecycleViewModel(loginViewModel)
+        
+        emailText = StringSubject(loginViewModel.emailText)
+        passwordText = StringSubject(loginViewModel.passwordText)
+        emailErrorText = StringObservable(loginViewModel.emailErrorText)
+        passwordErrorText = StringObservable(loginViewModel.passwordErrorText)
+        formFooterErrorText = StringObservable(loginViewModel.formFooterError)
+        
+        isLoading = BoolObservable(loginViewModel.isLoading)
+        isButtonEnabled = BoolObservable(loginViewModel.isButtonEnabled)
+    }
+    
+    var body: some View {
+        viewModel.lifecycleView { viewModel in
+            VStack {
+                TextField(viewModel.emailPlaceholder, text: $emailText.value)
+                    .autocapitalization(.none)
+                Text(emailErrorText.value)
+                TextField(viewModel.passwordPlaceholder, text: $passwordText.value)
+                    .autocapitalization(.none)
+                Text(passwordErrorText.value)
+                
+                Text(formFooterErrorText.value)
+                
+                if isLoading.value {
+                    ProgressView()
+                }
+                
+                Button(action: viewModel.login) {
+                    Text(viewModel.buttonTitle)
+                }.disabled(!isButtonEnabled.value)
+                Spacer()
+            }
+        }
+    }
+}
