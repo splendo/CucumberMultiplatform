768b9d27ae37217804394a0c9ee7ec4e1eadea12
corrado4eyes <corradoquattrocchi@hotmail.it>
2023-06-29 10:03:19 +0200

    Refactor cucumber module so to simplify it. Removed GherkinRunner and since android expects arguments for each string found in the Gherkin step refactored GherkinLambda

--- cucumber/src/commonMain/kotlin/com/corrado4eyes/cucumber/CucumberDefinition.kt
+++ cucumber/src/commonMain/kotlin/com/corrado4eyes/cucumber/CucumberDefinition.kt
@@ -1,7 +1,5 @@
 package com.corrado4eyes.cucumber
 
-import com.corrado4eyes.cucumber.tests.TestCase
-
 
 typealias Scenario = CucumberDefinition.Descriptive.Example
 
@@ -26,9 +24,51 @@ sealed class CucumberDefinition(val regex: String) {
     }
 
     sealed class Step(regex: String) : CucumberDefinition(regex) {
-        class Given(regex: String) : Step(regex)
-        class When(regex: String) : Step(regex)
-        class Then(regex: String) : Step(regex)
+        class Given(regex: String, lambda: GherkinLambda0) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class GivenSingle(regex: String, lambda: GherkinLambda1) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class GivenMultiple(regex: String, lambda: GherkinLambda2) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class When(regex: String, lambda: GherkinLambda0) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class WhenSingle(regex: String, lambda: GherkinLambda1) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class WhenMultiple(regex: String, lambda: GherkinLambda2) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class Then(regex: String, lambda: GherkinLambda0) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class ThenSingle(regex: String, lambda: GherkinLambda1) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
+        class ThenMultiple(regex: String, lambda: GherkinLambda2) : Step(regex) {
+            init {
+                given(regex, lambda)
+            }
+        }
     }
 
     sealed class SubStep(regex: String) : CucumberDefinition(regex) {
@@ -36,30 +76,48 @@ sealed class CucumberDefinition(val regex: String) {
     }
 }
 
-typealias TestCases = List<TestCase>
-
-interface GherkinRunner {
-    fun buildFeature()
+interface GherkinTestCase<T: GherkinLambda> {
+    val step: CucumberDefinition
+    val lambda: T
 }
 
 expect val EXPECT_VALUE_STRING: String
 
-expect class GherkinLambda constructor()
-expect class DefaultGherkinRunner(lambdaMaps: TestCases) : GherkinRunner
+interface GherkinLambda
 
-expect fun given(regex: String, lambda: GherkinLambda)
-infix fun GherkinLambda.Given(regex: String) {
-    given(regex, this)
-}
+expect class GherkinLambda0 : GherkinLambda
+expect class GherkinLambda1 : GherkinLambda
+expect class GherkinLambda2 : GherkinLambda
+//expect class GherkinLambda3 : GherkinLambda
+//expect class GherkinLambda4 : GherkinLambda
+//expect class GherkinLambda5 : GherkinLambda
+//expect class GherkinLambda6 : GherkinLambda
+//expect class GherkinLambda7 : GherkinLambda
+//expect class GherkinLambda8 : GherkinLambda
+//expect class GherkinLambda9 : GherkinLambda
 
-expect fun then(regex: String, lambda: GherkinLambda)
 
-infix fun String.Then(lambda: GherkinLambda) {
-    then(this, lambda)
-}
+expect fun given(regex: String, lambda: GherkinLambda0)
+expect fun given(regex: String, lambda: GherkinLambda1)
+expect fun given(regex: String, lambda: GherkinLambda2)
 
-expect fun `when`(regex: String, lambda: GherkinLambda)
+//infix fun GherkinLambdaMultiple.Given(regex: String) {
+//    given(regex, this)
+//}
 
-infix fun GherkinLambda.When(regex: String) {
-    `when`(regex, this)
-}
+expect fun then(regex: String, lambda: GherkinLambda0)
+expect fun then(regex: String, lambda: GherkinLambda1)
+expect fun then(regex: String, lambda: GherkinLambda2)
+
+
+//infix fun String.Then(lambda: GherkinLambdaMultiple) {
+//    then(this, lambda)
+//}
+
+expect fun `when`(regex: String, lambda: GherkinLambda0)
+expect fun `when`(regex: String, lambda: GherkinLambda1)
+expect fun `when`(regex: String, lambda: GherkinLambda2)
+
+//infix fun GherkinLambdaMultiple.When(regex: String) {
+//    `when`(regex, this)
+//}
