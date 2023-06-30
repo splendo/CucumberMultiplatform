f8ff58cdb922f8587d8c5208e8c3670168c06b36
Alex <alexandros@splendo.com>
2023-06-30 13:57:06 +0200

    Fixed Android App class setup for koin

--- android/src/main/java/com/corrado4eyes/cucumberplayground/android/CucumberApp.kt
+++ android/src/main/java/com/corrado4eyes/cucumberplayground/android/CucumberApp.kt
@@ -2,10 +2,12 @@ package com.corrado4eyes.cucumberplayground.android
 
 import android.app.Application
 import com.corrado4eyes.cucumberplayground.di.initKoin
+import com.splendo.kaluga.base.ApplicationHolder
 
 class CucumberApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        ApplicationHolder.application = this
         initKoin()
     }
 }
