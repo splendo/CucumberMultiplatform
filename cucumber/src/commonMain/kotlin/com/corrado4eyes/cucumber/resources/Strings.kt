7461f9721344148f012f2e9983f06825ad418a33
corrado4eyes <corradoquattrocchi@hotmail.it>
2023-09-13 09:12:02 +0200

    Add Strings

---
+++ cucumber/src/commonMain/kotlin/com/corrado4eyes/cucumber/resources/Strings.kt
@@ -0,0 +1,15 @@
+package com.corrado4eyes.cucumber.resources
+
+object Strings {
+    val loginScreenTitle = "Login screen"
+    val homeScreenTitle = "Home screen"
+
+    val loginScreenTag = "Login"
+    val homeScreenTag = "Home"
+
+    val loginButtonText = "Login"
+    val logoutButtonText = "Logout"
+
+    val emailTextFieldTag = "Email"
+    val passwordTextFieldTag = "Password"
+}
